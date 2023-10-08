0bdb24f17d180d25377701e0576720e77f4f2e47
JokNavi <JokNavi1@gmail.com>
2023-10-08 11:21:51 +0200

    Move CharBrightnesses example out of main fn

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,4 @@ use rusttype::Font;
 pub mod char_map;
 
 fn main() {
-    let chars = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
-    let font = &Font::try_from_bytes(include_bytes!("/home/joknavi/.local/share/fonts/RobotoMono-Regular.ttf")).unwrap();
-    let scale = 255; 
-    dbg!(CharBrightnesses::new(chars, font, scale));
 }

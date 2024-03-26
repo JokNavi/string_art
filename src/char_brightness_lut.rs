d2340e91ad622a699145daec9452f621bc1e5458
Navoso <VantorreWannes@gmail.com>
2024-03-26 18:28:52 +0100

    Create new CharDensityLut struct

--- src/char_brightness_lut.rs
+++ src/char_brightness_lut.rs
@@ -1,7 +1,16 @@
 pub const LUT_LENGTH: usize = u8::MAX as usize + 1;
 
 #[derive(Debug, PartialEq, Clone, Copy, Eq)]
-pub struct CharBrightnessLut {
+pub struct CharDensityLut {
     char_lut: [char; 256],
 }
 
+impl CharDensityLut {
+    pub fn new() -> Self {
+        todo!();
+    }
+
+    pub fn from_lut(lut: [char; 256]) -> Self {
+        Self { char_lut: lut }
+    }
+}

6ab1c8664067714e9c02bb43612d364d7214426e
Navoso <VantorreWannes@gmail.com>
2024-03-28 18:14:43 +0100

    Impl the Index trait for CharPixelDensityLut

--- src/char_density_lut.rs
+++ src/char_density_lut.rs
@@ -1,11 +1,13 @@
+use std::ops::Index;
+
 pub const LUT_LENGTH: usize = u8::MAX as usize + 1;
 
 #[derive(Debug, PartialEq, Clone, Copy, Eq)]
-pub struct CharDensityLut {
+pub struct CharPixelDensityLut {
     char_lut: [char; 256],
 }
 
-impl CharDensityLut {
+impl CharPixelDensityLut {
     pub fn new() -> Self {
         todo!();
     }
@@ -13,4 +15,12 @@ impl CharDensityLut {
     pub fn from_lut(lut: [char; 256]) -> Self {
         Self { char_lut: lut }
     }   
+}
+
+impl Index<u8> for CharPixelDensityLut {
+    type Output = char;
+
+    fn index(&self, index: u8) -> &Self::Output {
+        &self.char_lut[index as usize]
+    }  
 }

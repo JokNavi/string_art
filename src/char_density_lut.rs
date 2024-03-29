69211e2b3e8b572aa7f0ad3beec314812a0051d4
Navoso <VantorreWannes@gmail.com>
2024-03-29 14:13:58 +0100

    fix indexing bug

--- src/char_density_lut.rs
+++ src/char_density_lut.rs
@@ -4,7 +4,7 @@ pub const LUT_LENGTH: usize = u8::MAX as usize + 1;
 
 #[derive(Debug, PartialEq, Clone, Copy, Eq)]
 pub struct CharPixelDensityLut {
-    char_lut: [char; 256],
+    char_lut: [char; LUT_LENGTH],
 }
 
 impl CharPixelDensityLut {
@@ -12,9 +12,9 @@ impl CharPixelDensityLut {
         todo!();
     }
 
-    pub fn from_lut(lut: [char; 256]) -> Self {
+    pub fn from_lut(lut: [char; LUT_LENGTH]) -> Self {
         Self { char_lut: lut }
-    }   
+    }
 }
 
 impl Index<u8> for CharPixelDensityLut {
@@ -22,5 +22,5 @@ impl Index<u8> for CharPixelDensityLut {
 
     fn index(&self, index: u8) -> &Self::Output {
         &self.char_lut[index as usize]
-    }  
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,12 @@
 pub mod char_brightness_lut;
 pub mod char_density_lut;
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_index() {
+        let index = 255u8;
+        dbg!(index.saturating_add(1));
+    }
+}
